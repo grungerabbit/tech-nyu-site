07092cd7522c3f7597f16c1007dd2e194b3a832a
Cheryl Wu <grungerabbit@gmail.com>
2015-10-06 21:06:53 -0400

    fix paths

--- simple-cal/app/scripts/main.js
+++ simple-cal/app/scripts/main.js
@@ -1,10 +1,10 @@
 require.config({
   paths: {
-    'jquery': 'vendor/jquery/jquery',
+    'jquery': 'vendor/jquery/dist/jquery',
     'underscore': 'vendor/underscore-amd/underscore',
     'backbone': 'vendor/backbone-amd/backbone',
     'moment': 'vendor/moment/moment',
-    'clndr': 'vendor/clndr/clndr',
+    'clndr': 'vendor/clndr/clndr.min',
   }
 });
 
